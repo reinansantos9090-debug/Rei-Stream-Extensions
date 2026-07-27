a75eec612116ee1d3861a4d383a7a5aa84a167dc
reinansantos9090-debug <reinansantos9090@gmail.com>
2026-07-27 14:55:28 -0300

    Update ExampleProvider.kt

--- ExampleProvider/src/main/kotlin/com/example/ExampleProvider.kt
+++ ExampleProvider/src/main/kotlin/com/example/ExampleProvider.kt
@@ -4,17 +4,15 @@ import com.lagradost.cloudstream3.MainAPI
 import com.lagradost.cloudstream3.SearchResponse
 import com.lagradost.cloudstream3.TvType
 
-class ExampleProvider : MainAPI() { // All providers must be an instance of MainAPI
-    override var mainUrl = "https://example.com/" 
-    override var name = "Example provider"
-    override val supportedTypes = setOf(TvType.Movie)
+class ExampleProvider : MainAPI() {
+    override var mainUrl = "https://smartanimes.net"
+    override var name = "SmartAnimes"
+    override val supportedTypes = setOf(TvType.Anime)
 
-    override var lang = "en"
+    override var lang = "pt"
 
-    // Enable this when your provider has a main page
     override val hasMainPage = true
 
-    // This function gets called when you search for something
     override suspend fun search(query: String): List<SearchResponse> {
         return listOf()
     }
