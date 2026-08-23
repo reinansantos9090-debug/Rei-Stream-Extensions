d00bd3087959e1830635f03c549dd9731a7fa3c1
reinansantos9090-debug <reinansantos9090@gmail.com>
2026-08-23 09:44:47 -0300

    Update ExampleProvider.kt

--- ExampleProvider/src/main/kotlin/com/example/ExampleProvider.kt
+++ ExampleProvider/src/main/kotlin/com/example/ExampleProvider.kt
@@ -16,7 +16,7 @@ class SmartAnimes : MainAPI() {
         val document = app.get(mainUrl).document
         val home = mutableListOf<HomePageList>()
 
-        val items = document.select("article.item, div.anime-item").mapNotNull {
+        val items = document.select("article.item, div.anime-item, div.poster").mapNotNull {
             it.toSearchResult()
         }
 
@@ -28,7 +28,7 @@ class SmartAnimes : MainAPI() {
     }
 
     private fun Element.toSearchResult(): SearchResponse? {
-        val title = this.selectFirst("h3, .title, .entry-title")?.text() ?: return null
+        val title = this.selectFirst("h3, .title, .entry-title, a")?.text() ?: return null
         val href = this.selectFirst("a")?.attr("href") ?: return null
         val posterUrl = this.selectFirst("img")?.attr("src") ?: this.selectFirst("img")?.attr("data-src")
 
@@ -41,8 +41,43 @@ class SmartAnimes : MainAPI() {
         val url = "$mainUrl/?s=$query"
         val document = app.get(url).document
 
-        return document.select("article.item, div.anime-item").mapNotNull {
+        return document.select("article.item, div.anime-item, div.poster").mapNotNull {
             it.toSearchResult()
         }
+    }
+
+    override suspend fun load(url: String): LoadResponse? {
+        val document = app.get(url).document
+        val title = document.selectFirst("h1.entry-title, h1")?.text() ?: return null
+        val poster = document.selectFirst("div.poster img, img.wp-post-image")?.attr("src")
+        val description = document.selectFirst("div.entry-content, div.synopsis")?.text()
+
+        val episodes = document.select("ul.episodios li, div.episodio, a.episode").mapNotNull { ep ->
+            val epHref = ep.selectFirst("a")?.attr("href") ?: return@mapNotNull null
+            val epName = ep.selectFirst(".title, .num, a")?.text() ?: "Episódio"
+            Episode(epHref, epName)
+        }
+
+        return newAnimeLoadResponse(title, url, TvType.Anime) {
+            this.posterUrl = poster
+            this.plot = description
+            addEpisodes(DubStatus.Subbed, episodes)
+        }
+    }
+
+    override suspend fun loadLinks(
+        data: String,
+        isCasting: Boolean,
+        subtitleCallback: (SubtitleFile) -> Unit,
+        callback: (ExtractorLink) -> Unit
+    ): Boolean {
+        val document = app.get(data).document
+        val iframeUrl = document.selectFirst("iframe")?.attr("src")
+
+        if (!iframeUrl.isNullOrEmpty()) {
+            loadExtractor(iframeUrl, data, subtitleCallback, callback)
+        }
+
+        return true
     }
 }
