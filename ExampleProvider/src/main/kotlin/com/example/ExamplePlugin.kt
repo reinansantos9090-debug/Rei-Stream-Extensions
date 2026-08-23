1137b87be6833a8a9f8bebd0e3756edb69ec4ca2
reinansantos9090-debug <reinansantos9090@gmail.com>
2026-08-23 10:59:46 -0300

    Update ExamplePlugin.kt

--- ExampleProvider/src/main/kotlin/com/example/ExamplePlugin.kt
+++ ExampleProvider/src/main/kotlin/com/example/ExamplePlugin.kt
@@ -1,13 +1,8 @@
-package com.example
-
-import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
-import com.lagradost.cloudstream3.plugins.Plugin
-import android.content.Context
-
-@CloudstreamPlugin
-class ExamplePlugin: Plugin() {
-    override fun load(context: Context) {
-        // Registra o scraper do SmartAnimes no aplicativo
-        registerMainAPI(SmartAnimes())
-    }
+cloudstream {
+    // Aponta para a classe do plugin
+    setPluginClass("com.example.ExamplePlugin")
+    
+    // Nome que vai aparecer na lista do aplicativo
+    authors = listOf("reinansantos9090")
+    description = "Extensão do SmartAnimes"
 }
