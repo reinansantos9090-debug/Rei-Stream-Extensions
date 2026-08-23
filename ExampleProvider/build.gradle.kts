69278e3842baf5fae60c653c29a40ab548f39698
reinansantos9090-debug <reinansantos9090@gmail.com>
2026-08-23 09:28:04 -0300

    build.gradle.kts

--- ExampleProvider/build.gradle.kts
+++ ExampleProvider/build.gradle.kts
@@ -3,31 +3,16 @@ dependencies {
     implementation("androidx.recyclerview:recyclerview:1.3.2")
 }
 
-// Use an integer for version numbers
 version = 1
 
 cloudstream {
-    // All of these properties are optional, you can safely remove any of them.
-
-    description = "Lorem ipsum"
-    authors = listOf("Cloudburst", "Luna712")
-
-    /**
-    * Status int as one of the following:
-    * 0: Down
-    * 1: Ok
-    * 2: Slow
-    * 3: Beta-only
-    **/
-    status = 1 // Will be 3 if unspecified
-
-    tvTypes = listOf("Movie")
-
-    requiresResources = true
-    language = "en"
-
-    // Random CC logo I found
-    iconUrl = "https://upload.wikimedia.org/wikipedia/commons/2/2f/Korduene_Logo.png"
+    description = "Provedor de animes do SmartAnimes"
+    authors = listOf("Rei-Stream")
+    status = 1
+    tvTypes = listOf("Anime")
+    requiresResources = false
+    language = "pt-br"
+    iconUrl = "https://www.google.com/s2/favicons?domain=smartanimes.net&sz=128"
 }
 
 android {
